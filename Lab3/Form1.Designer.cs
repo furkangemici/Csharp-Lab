406467079b0c1922b01511226d6135f073c22de2
Furkan Gemici <furkngemici@gmail.com>
2025-03-21 15:30:25 +0300

    Lab3 content added

---
+++ Lab3/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace week5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.lblShift = new System.Windows.Forms.Label();
+            this.txtShift = new System.Windows.Forms.TextBox();
+            this.btnEncrypt = new System.Windows.Forms.Button();
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.lblCipher = new System.Windows.Forms.Label();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtPassword.Location = new System.Drawing.Point(62, 80);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(139, 32);
+            this.txtPassword.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(59, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Password:";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.radioButton1.Location = new System.Drawing.Point(349, 151);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(138, 24);
+            this.radioButton1.TabIndex = 3;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Ceaser Cipher";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // lblShift
+            // 
+            this.lblShift.AutoSize = true;
+            this.lblShift.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblShift.Location = new System.Drawing.Point(58, 180);
+            this.lblShift.Name = "lblShift";
+            this.lblShift.Size = new System.Drawing.Size(122, 24);
+            this.lblShift.TabIndex = 4;
+            this.lblShift.Text = "Cipher Code:";
+            // 
+            // txtShift
+            // 
+            this.txtShift.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtShift.Location = new System.Drawing.Point(62, 217);
+            this.txtShift.Name = "txtShift";
+            this.txtShift.Size = new System.Drawing.Size(139, 32);
+            this.txtShift.TabIndex = 5;
+            this.txtShift.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtShift_KeyDown);
+            // 
+            // btnEncrypt
+            // 
+            this.btnEncrypt.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnEncrypt.Location = new System.Drawing.Point(62, 304);
+            this.btnEncrypt.Name = "btnEncrypt";
+            this.btnEncrypt.Size = new System.Drawing.Size(139, 33);
+            this.btnEncrypt.TabIndex = 6;
+            this.btnEncrypt.Text = "Encrypt";
+            this.btnEncrypt.UseVisualStyleBackColor = true;
+            this.btnEncrypt.Click += new System.EventHandler(this.btnEncrypt_Click);
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.lblMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblMessage.Location = new System.Drawing.Point(318, 304);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(289, 38);
+            this.lblMessage.TabIndex = 7;
+            this.lblMessage.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.lblMessage.Visible = false;
+            // 
+            // lblCipher
+            // 
+            this.lblCipher.AutoSize = true;
+            this.lblCipher.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblCipher.Location = new System.Drawing.Point(345, 368);
+            this.lblCipher.Name = "lblCipher";
+            this.lblCipher.Size = new System.Drawing.Size(0, 24);
+            this.lblCipher.TabIndex = 8;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.radioButton2.Location = new System.Drawing.Point(349, 192);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(74, 24);
+            this.radioButton2.TabIndex = 9;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "ROT2";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(684, 458);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.lblCipher);
+            this.Controls.Add(this.lblMessage);
+            this.Controls.Add(this.btnEncrypt);
+            this.Controls.Add(this.txtShift);
+            this.Controls.Add(this.lblShift);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPassword);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Label lblShift;
+        private System.Windows.Forms.TextBox txtShift;
+        private System.Windows.Forms.Button btnEncrypt;
+        private System.Windows.Forms.Label lblMessage;
+        private System.Windows.Forms.Label lblCipher;
+        private System.Windows.Forms.RadioButton radioButton2;
+    }
+}
+
---
+++ Lab3/Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace week5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // GroupBox ekle
+            GroupBox groupBox = new GroupBox();
+            groupBox.Text = "Encryption System";
+            groupBox.Location = new Point(30, 10); 
+            groupBox.Size = new Size(500, 400); 
+
+            
+            txtPassword.UseSystemPasswordChar = true;
+            lblShift.Visible = false;
+            txtShift.Visible = false;
+
+            // GroupBox elemanları
+            groupBox.Controls.Add(txtPassword);
+            groupBox.Controls.Add(lblShift);
+            groupBox.Controls.Add(txtShift); 
+            groupBox.Controls.Add(radioButton1); 
+            groupBox.Controls.Add(radioButton2); 
+            groupBox.Controls.Add(btnEncrypt); 
+            groupBox.Controls.Add(lblMessage); 
+            groupBox.Controls.Add(lblCipher); 
+
+            // Forma GroupBoxı ekle
+            Controls.Add(groupBox);
+
+           
+            txtShift.Visible = false;
+            lblShift.Visible = false;
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            txtShift.Visible = radioButton1.Checked;
+            lblShift.Visible = radioButton1.Checked;
+        }
+
+        public static void Encipher(char[] input, int key, ref bool success, Label successLabel, out string encryptedMessage)
+        {
+            encryptedMessage = string.Empty;
+
+            if (input.Length == 0)
+            {
+                success = false;
+                successLabel.Text = "Input is empty";
+                return;
+            }
+
+            key = key % 26; // Harfler için kaydırma
+
+            foreach (char ch in input)
+            {
+                if (char.IsLetter(ch)) // Harfse şifrele
+                {
+                    encryptedMessage += Cipher(ch, key);
+                }
+                else if (char.IsDigit(ch)) // Rakam ise toplama yap
+                {
+                    encryptedMessage += ShiftNumber(ch, key);
+                }
+                else // Özel karakterse olduğu gibi bırak
+                {
+                    encryptedMessage += ch;
+                }
+            }
+
+            success = true;
+            successLabel.Text = "Success";
+        }
+
+        public static char Cipher(char ch, int key)
+        {
+            if (char.IsLetter(ch))
+            {
+                char d = char.IsUpper(ch) ? 'A' : 'a';
+                ch = (char)((((ch - d + key) % 26 + 26) % 26) + d);// mod içine aldık
+            }
+            return ch;
+        }
+
+        public static string ShiftNumber(char num, int key)
+        {
+            int digit = num - '0';
+            int newDigit = digit + key;
+            return newDigit.ToString(); // Yeni rakamı string olarak döndür
+        }
+
+        private void btnEncrypt_Click(object sender, EventArgs e)
+        {
+            string password = txtPassword.Text;
+            int shift = 1;
+
+            bool success = false;
+            string encryptedMessage;
+
+            lblMessage.Visible = true;
+
+            if (radioButton1.Checked) // Caesar Cipher seçildiyse
+            {
+                if (int.TryParse(txtShift.Text, out shift))
+                {
+                    // Kullanıcı geçerli bir sayı girdiyse devam et
+                }
+                else
+                {
+                    lblMessage.BackColor = Color.Red;
+                    lblMessage.Text = "Hata: Geçersiz Kaydırma Kodu.";
+                    lblCipher.Text = "";
+                    return;
+                }
+            }
+            else if (radioButton2.Checked) // ROT2 seçildiyse
+            {
+                shift = 2; // Sabit olarak 2 kaydır
+            }
+
+            char[] passwordArray = password.ToCharArray();
+            Encipher(passwordArray, shift, ref success, lblMessage, out encryptedMessage);
+
+            lblMessage.Visible = true;
+
+            if (success)
+            {
+                lblMessage.BackColor = Color.Green;
+                lblMessage.Text = "Success!";
+                lblCipher.Text = "Cipher: " + encryptedMessage;
+            }
+            else
+            {
+                lblMessage.BackColor = Color.Red;
+                lblMessage.Text = "Hata: Şifreleme Başarısız!";
+                lblCipher.Text = "";
+            }
+        }
+
+        private void txtShift_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                btnEncrypt.PerformClick();
+            }
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            txtShift.Visible = false;
+            lblShift.Visible = false;
+        }
+    }
+}
